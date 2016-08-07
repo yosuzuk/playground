935b4d947829217b9876f82373e785c49339e5df
yosuzuk <yosuzuk@users.noreply.github.com>
2016-08-07 16:21:37 +0200

    Webpack config

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# playground
+# Webpack1 Sample
+
+Proof of concept: Gradually migrating from RequireJS AMD bower to Webpack ES6 npm
+
+## Motivation
+
+- Implementing new modules in ES6 while reusing existing AMD modules
+- Easy support for multiple versions of the same module on the same page
+- Automated wireing for 3rd party libraries without custom 'copy' tasks
+
+## Conditions
+
+- Working with existing folder structure
+- Working with existing modules written in AMD
+- Working with existing require statements for static assets (e.g. templates)
+- Working with existing asynchronous require statements (require([...], f))
+- Working with path alias
+- Working with bower dependencies
+
+## Steps
+
+Init project
+
+    npm init
+
+Install Webpack
+
+    npm install webpack --save-dev
+
+(missing webpack in PATH, TODO solve, Workaround via script entry in package.json)
+
+Create `webpack.config.js`
+
+Install Babel and its loader and polyfill
+
+    npm install --save-dev babel-core babel-preset-es2015 babel-loader babel-polyfill
+
+Create `.babelrc`
+
+    { "presets": [ "es2015" ] }
+
+Move as many 3rd party libraries as possible from bower.json to package.json
+
+    npm install --save jquery
+
+Tell jshint to allow ES6
+
+    { "esversion": 6 }
+
+( '"esnext": true' is deprecated )
+
+Install raw-loader and configure an alias 'text' pointing to the raw loader
+
+    npm install --save-dev raw-loader
+
+(for existing require statements using RequireJS's text plugin, e.g. require('text!foo.html'))
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const isDebug = global.DEBUG === false ? false : !process.argv.includes('--release');
+const isVerbose = process.argv.includes('--verbose') || process.argv.includes('-v');
+
+const config = {
+    debug: isDebug, // switch loaders to debug or release mode
+    entry: {
+        products: './assets/js/modules/views/products/indexES6.js',
+        productCategories: './assets/js/modules/views/productCategories/indexAMD.js'
+    },
+    output: {
+        path: __dirname + '/webroot/dist',
+        filename: 'js/modules/views/[name]/index.js?[hash]',
+        publicPath: 'dist/js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+        }]
+    },
+    resolveLoader: {
+        alias: {
+            text: 'raw'
+        }
+    },
+    resolve: {
+        modulesDirectories: ['node_modules', 'bower_components'], // adds bower support
+        root: path.resolve(__dirname),
+        alias: {
+            products: 'assets/js/modules/views/products',
+            productCategories: 'assets/js/modules/views/productCategories'
+        }
+    },
+    plugins: [
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('.bower.json', ['main'])
+        )
+        //new webpack.NormalModuleReplacementPlugin(/^(domReady\!)$/, 'TODO define')
+    ],
+    node: {
+        fs: 'empty', // mocks for when node.js specific modules may be required
+        net: 'empty',
+        tls: 'empty'
+    }
+};
+
+// Optimize the bundle in release (production) mode
+if (!isDebug) {
+    config.plugins.push(new webpack.optimize.DedupePlugin());
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: isVerbose
+        }
+    }));
+    config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
+}
+
+module.exports = config;
